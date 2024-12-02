6b588f62012b2a0a54ccf018af225da0c91cf094
Satynarayan Maurya <123947617+SatynarayanMaurya@users.noreply.github.com>
2024-12-02 10:04:11 +0530

    Update NoInternet.jsx

--- frontend/src/Pages/HomePage/NoInternet.jsx
+++ frontend/src/Pages/HomePage/NoInternet.jsx
@@ -3,10 +3,10 @@ import noInternetImage from "../../Assests/Extras/noInternet.png"
 function NoInternet() {
   return (
     <div className='bg-[#202124] h-screen flex justify-center items-center'>
-      <div className='w-[500px] h-[400px] text-[#9AA0A6] -mt-36'>
+      <div className='lg:w-[500px] w-[320px] text-[#9AA0A6] -mt-36'>
         <img src={noInternetImage} alt="" className='w-[500px] object-cover' />
-        <p className='font-semibold text-3xl'>No Internet</p>
-        <div className='flex flex-col gap-2 text-lg mt-2'>
+        <p className='font-semibold text-3xl lg:mt-0 mt-4'>No Internet</p>
+        <div className='flex flex-col gap-2 text-lg lg:mt-2 mt-4'>
             <p>Try:</p>
             <ul className='pl-10 list-disc flex flex-col gap-2 '>
                 <li>Checking the network cables, modem, and router</li>
